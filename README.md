a12ce2ad992e0133a294491fac1dbd2bf7e8e3ce
Sy Tran <sy.tran@unified.com>
2019-12-16 19:42:23 +0700

    Tunnel util

---
+++ README.md
@@ -0,0 +1,47 @@
+# Tunnel
+## Usage
+![live demo](img/usage.gif)
+
+- Create database and insert default data `tunnel init [-f FILE]`
+- Add more connection `tunnel add -n NAME -H HOST -p PORT -l LOCAL_PORT -g GATEWAY`
+- List connections `tunnel ls [-s {up,down,all}]`
+- Connect/Disconnect tunnel ` tunnel <up|down> names [names ...]`
+
+```sh
+└─ $ tunnel ls
+redshift:5439 ✔️
+rds:5438 ❌
+neo4j:7474 ❌
+scheduler:9073 ❌
+
+└─ $ tunnel up rds
+Activate tunnel rds
+Activated.
+
+└─ $ tunnel down redshift
+Disconnected from redshift
+
+└─ $ tunnel ls -s up
+rds:5438 ✔️
+
+└─ $ tunnel ls -s down
+redshift:5439 ❌
+neo4j:7474 ❌
+scheduler:9073 ❌
+```
+
+## Require
+- python3
+
+## Install
+- Add contain folder to `PATH`
+- Auto completion ``echo 'eval "`tunnel complete`"' >> ~/.bashrc``
+- Add connections into `config.ini` following format then `tunnel init`.
+
+## Platform
+
+| | Linux | Window | Mac |
+|--|--|--|--|
+|Tested| ✔️  | ❌ | ❌ |
+
+# OTHERS
---
+++ tunnel-complete.sh
@@ -0,0 +1,45 @@
+_tunnel_py()
+{
+    local opts="init add ls up down" i=1
+
+    case ${COMP_WORDS[i]} in
+        init)
+            if [[ "$COMP_CWORD" -ge "$i" ]]; then
+                opts=""
+            else
+                opts="-h --help"
+            fi
+            ;;
+        add)
+            if [[ $(( COMP_CWORD % 2 )) == 0 ]]; then
+                opts="-n -H -p -l -g --name --host --port --local-port --gateway"
+            else
+                opts=""
+            fi
+            ;;
+        ls)
+            if [[ $i -eq "$COMP_CWORD - 1" ]]; then
+                opts="-s"
+            else
+                ((i++))
+                if [[ "$i" -eq "$COMP_CWORD - 1" ]]; then
+                    opts="all up down"
+                else
+                    opts=""
+                fi
+            fi
+            ;;
+        up)
+            opts=$(tunnel ls -s down | cut -d ':' -f1)
+            ;;
+        down)
+            opts=$(tunnel ls -s up | cut -d ':' -f1)
+            ;;
+    esac
+
+    COMPREPLY=( $(compgen -W "$(echo $opts | xargs -n1 | sort -g | xargs)" -- "${COMP_WORDS[COMP_CWORD]}") )
+    return 0
+}
+complete -F _tunnel_py tunnel
+# Run this >>>>>>>>>>>>>>>>
+# eval "`tunnel complete`"
